ef63f824fa63a96ca8f12559c9c3c7189c47e9a1
Michael Bagwell <mpbagwell@gmail.com>
2020-08-15 16:21:46 -0500

    Register createOrder as a submit handler

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,8 +1,14 @@
 (function (window) {
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
   var myTruck = new Truck('ncc-1701', new DataStore());
+  var formHandler = new FormHandler(FORM_SELECTOR);
   window.myTruck = myTruck;
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
 }) (window);
